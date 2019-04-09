8097a8cc34efdf3c3a7ea828a1400da35ce3a009
Miklós Antal <mr.antal@gmail.com>
2019-04-09 14:21:52 +0200

    remove non working bin option

--- cli.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-const getEnvPaths = require('./get-env-paths');
-
-require('./main').config(getEnvPaths());
--- config.js
+++ config.js
@@ -1,5 +1,19 @@
 'use strict';
 
-const getEnvPaths = require('./get-env-paths');
+require('./main').config(getPathsFromEnv() || getPathsFromCli() || '.env');
 
-require('./main').config(getEnvPaths());
+function getPathsFromEnv() {
+  return process.env.DOTENV_PATHS || null
+}
+
+function getPathsFromCli() {
+  const re = /^dotenv_paths=(.+)$/;
+  const matchingOption = process.argv.find(arg => arg.match(re));
+
+  if (matchingOption) {
+    const matches = matchingOption.match(re);
+    return matches[1];
+  } else {
+    return null;
+  }
+}
